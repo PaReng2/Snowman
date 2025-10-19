1093f36c5df79edeaa065b3332bc74cb9c272e60
PaReng2 <korreppsj@naver.com>
2025-10-20 01:56:13 +0900

    [Feat] 플레이어 공격 추가

---
+++ Project_Snowman/Assets/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float damage;
+
+    private PlayerController playerController;
+    private float lifeTime;
+
+    private void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+        lifeTime = playerController.attackRange;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject, lifeTime);
+        }
+
+    }
+}
--- Project_Snowman/Assets/Scripts/PlayerController.cs
+++ Project_Snowman/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 3f;
+    public Transform firePoint;
+    public GameObject bulletPrefab;
     float moveX;
     float moveZ;
     Animator animator;
+    float curLeftAttackTime;
+    float AttackRate;
+    public float attackRange;
+    
+    Vector3 moveVec;
+
+    // 민감도
+    public float rotationSpeed = 300f;
+
+    // 마우스 입력 값
+    private Vector2 lookInput;
+    // Y 축 회전 각도 (수직 회전 제한을 위해 필요)
+    private float cameraVerticalAngle = 0f;
 
     Rigidbody rb;
 
@@ -16,6 +30,8 @@ public class PlayerController : MonoBehaviour
     {
         animator = GetComponentInChildren<Animator>();  
         rb = GetComponent<Rigidbody>();
+        AttackRate = 2.3f;
+        attackRange = 2f;
     }
 
     // Update is called once per frame
@@ -23,6 +39,31 @@ public class PlayerController : MonoBehaviour
     {
         Move();
         Jump();
+        
+
+        // curLeftAttackTime이 0보다 클 때만 감소시켜서 음수가 되는 것을 방지합니다.
+        if (curLeftAttackTime > 0)
+        {
+            curLeftAttackTime -= Time.deltaTime;
+        }
+
+        // 이 아래는 원래 코드와 동일합니다.
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (curLeftAttackTime <= 0)
+            {
+                    Attack();
+                    // 공격 후 쿨다운 시간을 AttackRate로 재설정합니다.
+                    curLeftAttackTime = AttackRate;
+            }
+            else
+            {
+                Debug.Log("재정비중");
+                return;
+            }
+        }
+
+
     }
 
     void Move()
@@ -30,7 +71,7 @@ public class PlayerController : MonoBehaviour
         moveX = Input.GetAxis("Horizontal");
         moveZ = Input.GetAxis("Vertical");
         
-        Vector3 moveVec = new Vector3(moveX, 0, moveZ).normalized;
+        moveVec = new Vector3(moveX, 0, moveZ).normalized;
         transform.position += moveVec * moveSpeed * Time.deltaTime;
 
         animator.SetBool("isMove", moveVec != Vector3.zero);
@@ -43,6 +84,12 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
         }
+    }
+    void Attack()
+    {
+        GameObject intantBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody bulletRigid = intantBullet.GetComponent<Rigidbody>();
+        bulletRigid.velocity = firePoint.forward * 50;
     }
     
 }
